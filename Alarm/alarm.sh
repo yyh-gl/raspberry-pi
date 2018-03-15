d578c853c997da0e853327a43efe9089be729ba3
yy_h <yhonda@mikilab.doshisha.ac.jp>
2018-03-15 22:26:01 +0900

    Fix derectory tree

--- Alarm/alarm.sh
+++ Alarm/alarm.sh
@@ -1,5 +1,5 @@
 
 ## atコマンドによって時間を指定
 cd /home/rasp-yyh/smart-home/Alarm &&
-echo "/usr/bin/mpg321 ./sound.mp3 ; usr/bin/mpg321 ./sound.mp3 ; ruby ../WeatherForecast/weather.rb" | at $1
+echo "/usr/bin/mpg321 ./music/ff.mp3 ; ruby ../WeatherForecast/weather.rb" | at $1
 
