87769becd1d5e7faf3a78b115fa793269ce590cf
yy_h <yhonda@mikilab.doshisha.ac.jp>
2018-03-21 00:00:07 +0900

    Test Remocon

--- Remocon/IR-remocon02-commandline.py
+++ Remocon/IR-remocon02-commandline.py
@@ -198,14 +198,13 @@ while True:
            #print("sys.argv[2]=",sys.argv[2])
            
            #print("trans_start")
-           # block2 = sys.argv[2]
-           # #print(block2)
-           # trans_command(block2)
+           #block2 = sys.argv[2]
            filepath = '/home/rasp-yyh/smart-home/Remocon/' + sys.argv[2]
            f = open(filepath)
-           data1 = f.read()  # ファイル終端まで全て読んだデータを返す
+           block2 = f.read()  # ファイル終端まで全て読んだデータを返す
            f.close()
-           print(data1)
+           #print(block2)
+           trans_command(block2)
            break   
 
     print("end")
