967fd353b4d1aca2e1bc1ea37428504fdf3756cb
yy_h <yhonda@mikilab.doshisha.ac.jp>
2018-03-20 21:07:32 +0900

    Add UGI

---
+++ UGI/ugi.rb
@@ -0,0 +1,19 @@
+require 'slack-notifier'
+
+
+FILEPATH = '/Users/Y_h/RaspberryPi/UGI/ugi.txt'
+
+new_global_ip = `curl ifconfig.io`.chomp
+
+file = File.open(FILEPATH, 'r')
+current_global_ip = file.gets.chomp
+file.close
+
+if (new_global_ip != current_global_ip)
+  file = File.open(FILEPATH, 'w')
+  file.puts(new_global_ip)
+  file.close
+
+  slack = Slack::Notifier.new(ENV['UGI_SLACK_WEBHOOK_URL'])  
+  slack.ping(new_global_ip)
+end
